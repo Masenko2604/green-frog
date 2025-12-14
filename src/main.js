1b83ac11fa8d82d3009e96c3a593cee351b33c56
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2025-12-14 23:21:12 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import('./js/mobile-menu');
 import './js/carousel-settings.js';
 import('./js/animation');
-import { translations } from "./lang";
 
-function applyTranslations(lang) {
-  for (const key in translations[lang]) {
-    const elements = document.getElementsByClassName(key);
-    for (const el of elements) {
-      el.innerText = translations[lang][key];
-    }
-  }
-}
 
 
