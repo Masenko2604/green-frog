3ae209eb304feb95429c806e2471e00ef6c5681a
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2025-12-22 18:40:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,27 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => {
-  return {
-    base: '/green-frog/',
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig(({ command }) => ({
+  base: '/green-frog/',
+
+  root: 'src',
+
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
+
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
     },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-      },
-      outDir: '../dist',
-      emptyOutDir: true,
-    },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-      SortCss({ sort: 'mobile-first' }),
-    ],
-  };
-});
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+
+  plugins: [
+    injectHTML(),
+    command === 'serve' && FullReload(['./src/**/*.html']),
+    SortCss({ sort: 'mobile-first' }),
+  ].filter(Boolean),
+}));
