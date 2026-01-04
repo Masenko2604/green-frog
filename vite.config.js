60f27153aef27a90891adfea205ca92e3ebac75f
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2026-01-04 13:36:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -49,32 +49,3 @@ export default defineConfig(({ command }) => {
 });
 
 
-// import { defineConfig } from 'vite';
-// import { glob } from 'glob';
-// import injectHTML from 'vite-plugin-html-inject';
-// import FullReload from 'vite-plugin-full-reload';
-// import SortCss from 'postcss-sort-media-queries';
-
-// export default defineConfig(({ command }) => ({
-//   base: '/green-frog/',
-
-//   root: 'src',
-
-//   define: {
-//     [command === 'serve' ? 'global' : '_global']: {},
-//   },
-//    build: {
-//     sourcemap: false,
-//     rollupOptions: {
-//       input: glob.sync('./src/*.html'),
-//     },
-//     outDir: '../dist',
-//     emptyOutDir: true,
-//   },
-
-//   plugins: [
-//     injectHTML(),
-//     command === 'serve' && FullReload(['./src/**/*.html']),
-//     SortCss({ sort: 'mobile-first' }),
-//   ].filter(Boolean),
-// }));
