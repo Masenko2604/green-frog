29328563426f8126193c0540cd196dd70cd5c245
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2026-01-04 11:59:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,77 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => ({
-  base: '/green-frog/',
-
-  root: 'src',
-
-  define: {
-    [command === 'serve' ? 'global' : '_global']: {},
-  },
-   build: {
-    sourcemap: false,
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
+export default defineConfig(({ command }) => {
+  return {
+    base: '/green-frog/',
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
 
-  plugins: [
-    injectHTML(),
-    command === 'serve' && FullReload(['./src/**/*.html']),
-    SortCss({ sort: 'mobile-first' }),
-  ].filter(Boolean),
-}));
+
+// import { defineConfig } from 'vite';
+// import { glob } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+// import SortCss from 'postcss-sort-media-queries';
+
+// export default defineConfig(({ command }) => ({
+//   base: '/green-frog/',
+
+//   root: 'src',
+
+//   define: {
+//     [command === 'serve' ? 'global' : '_global']: {},
+//   },
+//    build: {
+//     sourcemap: false,
+//     rollupOptions: {
+//       input: glob.sync('./src/*.html'),
+//     },
+//     outDir: '../dist',
+//     emptyOutDir: true,
+//   },
+
+//   plugins: [
+//     injectHTML(),
+//     command === 'serve' && FullReload(['./src/**/*.html']),
+//     SortCss({ sort: 'mobile-first' }),
+//   ].filter(Boolean),
+// }));
