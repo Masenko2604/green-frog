6eee16fcc5233c74657e17aecd76ea1e76eeb2b3
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2026-08-27 13:52:50 +0000

    Deploying to gh-pages from @ Masenko2604/green-frog@71140bb890c963ac19f511bd6d627b502fe1ed9f 🚀

--- questions.js
+++ questions.js
@@ -1,2 +1,2 @@
-import"./assets/lang-switcher-Druz6TZX.js";import"./assets/vendor-BNqRyOic.js";document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".faq-item").forEach(t=>{t.querySelector(".faq-question").addEventListener("click",()=>{t.classList.toggle("is-open")})})});
+import"./assets/lang-switcher-D015c5nx.js";import"./assets/vendor-BNqRyOic.js";document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".faq-item").forEach(t=>{t.querySelector(".faq-question").addEventListener("click",()=>{t.classList.toggle("is-open")})})});
 //# sourceMappingURL=questions.js.map
